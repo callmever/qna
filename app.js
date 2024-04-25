8d0e539b3589ac9a85c43c15c9a9ea3c06b5b032
callmecua1 <josuaoconner2321@gmail.com>
2024-04-25 14:46:26 +0700

    edit

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
 const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 const port = 5500;
-
-
 
 app.use(express.static("public"));
 
-app.use('/qna', express.static(path.join(__dirname + '/public')));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.use((req, res) => {
-    res.status(404)
-    res.send(`<h3>Error : Wrong path or the file maybe not exist</h3>`);
-})
+  res.status(404);
+  res.send(`<h3>Error : Wrong path or the file maybe not exist</h3>`);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-
-
-
-
 // app.get('/printer', (req, res) => {
 //     const filePath = path.join(__dirname, 'public', 'categories', 'printer.html')
 //     res.sendFile(filePath)
 // })
-
-
-
